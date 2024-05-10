e060cee7c07dce56167b1ffbb60b26234092ee37
bwalter <bwalter@precognox.com>
2024-05-10 17:41:36 +0200

    Fix login

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -1,22 +1,29 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage extends PageBase {
-
-    public LoginPage(WebDriver driver) {
-        super(driver);
-    }
 
     private By usernameLocator = By.id("login-usr");
     private By passwordLocator = By.id("login-psw");
     private By loginButtonLocator = By.id("login-submit");
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
 
     public void login() {
         this.waitAndReturnElement(usernameLocator).sendKeys("testingselenium309@gmail.com");
 
         this.waitAndReturnElement(passwordLocator).sendKeys("C7V43WgyeIWHWYFgI13V");
 
-        this.waitAndReturnElement(loginButtonLocator).click();
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(loginButtonLocator));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        implicitWait();
+        js.executeScript("arguments[0].click();", loginButton);
+
     }
 }
